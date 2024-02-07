3c97ad0bb6e7aa953e2c7cb66f69523a38bc6693
SeoYoon <34805713+s-y00n3@users.noreply.github.com>
2024-02-07 13:27:23 +0900

    # declare request example data

--- main.py
+++ main.py
@@ -1,22 +1,91 @@
-from typing import Annotated
-from fastapi import FastAPI, Path, Query
+from typing import Annotated, List
+
+from fastapi import FastAPI, Path, Body
+from pydantic import BaseModel, Field, HttpUrl
 
 app = FastAPI()
 
-@app.get("/items/{item_id}")
-# async def read_items(
-#     item_id : int = Path(title="the id of the item to get"),
-#     q: Annotated[str | None, Query(alias="item-query")] = None,
-# ) :
 
-# 숫자 검증 (ge, gt, le, lt)
-async def read_items(
-    item_id: Annotated[int, Path(title="the id of the item to get", gt = 0, le = 1000)],
-    q: str,
-    size: float = Query(gt= 0, lt=10.5), # float 값에도 동작
-):
-    results = {"item_id" : item_id}
-    if q:
-        results.updatE({"q" : q})
-    return results
+class Image(BaseModel):
+    url:HttpUrl
+    name:str
     
+class Item(BaseModel):
+    name: str
+    #description: str | None = None
+    description: str | None = Field(default=None, title="the description of the item", max_length=300),
+    price : float = Field(gt=0, description="the price must be greater than zero")
+    tax : float | None = None,
+    #tags: List[str] = []
+    tags: set[str] = set()
+    #image: Image | None = None
+    #images: list[Image] | None = None
+    model_config = {
+        "json_schema_extra" : {
+            "examples" : [
+                {
+                    "name" : "foo",
+                    "description" : "a nice item",
+                    "price" : 24.5,
+                    "tax" : 2.1
+                }
+            ]
+        }
+    }
+
+class User(BaseModel):
+    usernmae: str
+    full_name: str | None = None
+
+
+@app.put("/items/{item_id}")
+# async def update_item(
+#     item_id: Annotated[int, Path(title="the id of the item to get", ge=0, le = 1000)],
+#     q: str | None = None,
+#     item: Item | None = None,
+# ):
+# if q:
+#         results.update({"q" : q})
+#     if item:
+#         results.update({"item" : item})
+#     return results
+
+# importance : convert data types, validate, document
+# async def update_item(item_id: int, item: Item, user: User):
+#     results = {"item_id" : item_id, "item": item, "user" : user}
+#     return results
+
+# async def update_item(
+#     item_id : int,
+#     item: Item,
+#     user: User,
+#     importance: Annotated[int, Body(gt=0)],
+#     q: str | None = None,
+# ) :
+#     results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# async def update_item(item_id: int, item: Item):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+async def update_item(
+    item_id: int,
+    item: Annotated[
+        Item,
+        Body(
+            examples=[
+                {
+                    "name":"foo",
+                    "description" : "a very nice item!",
+                    "price" : 25.4,
+                    "tax" : 1.5,
+                }
+            ],
+        ),
+    ],
+) :
+    results = {"item_id" : item_id, "item" : item}
+    return results
