9c3941eecee4246bc280fdf4ca80a4932f7461ad
SeoYoon <34805713+s-y00n3@users.noreply.github.com>
2024-02-07 10:48:01 +0900

    # 쿼리 매개변수 , 문자열 검증

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import List, Union
 
 from fastapi import FastAPI, Query
 from pydantic import BaseModel
@@ -48,9 +48,28 @@ async def get_model(model_name: ModelName) :
 async def read_file(file_path: str):
     return {"file_path": file_path}
 
+# max_length, min_length : 쿼리 최대, 최소 길이 설정
+# pattern : 정규식
 @app.get("/items")
-async def read_items(q: Union[str, None] = Query(default=None, min_length=3, max_length=50)):
-    results = {"items" : [{"item_id":"Foo"}, {"item_id" : "Bar"}]}
+#async def read_items(q: Union[str, None] = Query(default=None, min_length=3, max_length=50, pattern="^fixedquery&")):
+#async def read_items(q: str = Query(default="fixedquery", min_lenth = 3)):
+
+# 쿼리 매개변수 리스트 / 다중값
+#async def read_items(q: Union[List[str], None] = Query(default=None)) : 
+
+# 쿼리 다중값 기본값 설정
+#async def read_items(q: List[str] = Query(default=["foo", "bar"])) :
+
+# List[str] 대신 list 사용
+#async def read_items(q: list = Query(default=[])):
+
+# 메타데이터 선언
+#async def read_items(q: Union[str, None] = Query(default=None, title="query string", description="description", min_length=3)) : 
+
+# 별칭 매개변수
+# deprecated = true로 설정하면 매개변수 사용되지 않음을 알림
+async def read_items(q:Union[str, None] = Query(default=None, alias="item-query")) :
+    results = {"items" : [{"item_id": "Foo"} , {"item_id" : "Bar"}]}
     if q:
         results.update({"q" : q})
     return results
